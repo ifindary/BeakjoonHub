93a3eb38654518aefc794a1388964b82fed1b053
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-16 01:10:50 +0900

    [Silver V] Title: 단어 정렬, Time: 72 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,31 +1,13 @@
 import sys
 
-def quick_sort(a, left:int, right:int):    
-    pl = left
-    pr = right
-    x = a[(left+right)//2]
-
-    while pl <= pr:
-        while a[pl] < x : pl += 1
-        while a[pr] > x : pr -= 1
-        if pl <= pr:
-            a[pl], a[pr] = a[pr], a[pl]
-            pl += 1
-            pr -= 1
-    
-    if left < pr: quick_sort(a, left, pr)
-    if right > pl: quick_sort(a, pl, right)
-
 n=int(sys.stdin.readline().strip())
 
-input_str=(str(sys.stdin.readline().strip()) for _ in range(n))
+a=set(str(sys.stdin.readline().strip()) for _ in range(n))
 
-set_str = set(input_str)
-re_str = list(set_str)
+words=list(a)
 
-# 소문자로 준다고 했지만 소문자로 바꿔서 생각하겠다고 명시하였음 (공부용)
-temp_str = sorted(re_str, key=str.lower)
-new_str = sorted(temp_str, key=len)
+words.sort()
+words.sort(key=len)
 
-for i in new_str:
-    print(i)
+for i in words:
+    sys.stdout.write(i+'\n')

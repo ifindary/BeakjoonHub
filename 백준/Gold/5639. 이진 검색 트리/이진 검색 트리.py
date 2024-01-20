370e04b583ea61b67b51ccec75aad7307a18e385
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-21 01:08:47 +0900

    [Gold V] Title: 이진 검색 트리, Time: 56 ms, Memory: 33388 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,37 +1,26 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readlines
 sys.setrecursionlimit(10 ** 9)
 
-numbers = []
+numbers = list(map(int, input()))
 
-# EOF 에러 처리 # int 안 붙이면 빈값도 계속 받음
-while True:
-    try:
-        numbers.append(int(input().rstrip()))
-    except:
-        break
-
-def postorder(s_idx, e_idx): # (시작 인덱스, 종료 인덱스)
-    if s_idx > e_idx:
+def dfs(s_idx, e_idx):
+    if e_idx - s_idx == 1: # 검사할 서브 트리의 노드가 하나인 경우
+        print(numbers[s_idx])
         return
     
-    # 오른쪽 서브 트리의 시작 인덱스를 끝값까지 두기
-    # 모든 노드가 왼쪽 서브 트리라고 가정
-    right_idx = e_idx + 1
-
-    for i in range(s_idx+1, e_idx+1):
-        if numbers[i] > numbers[s_idx]: # 루트 노드보다 큰값을 발견하면
-            right_idx = i # 그 인덱스부터 오른쪽 서브 트리임을 알 수 있으니까 저장
-            break
-
-    # 왼쪽 서브 트리를 대상으로 다시 탐색
-    postorder(s_idx+1, right_idx-1)
-
-    # 왼쪽이 다 끝나면 서브 트리를 대상으로 다시 탐색
-    postorder(right_idx, e_idx)
-
+    # 오른쪽 서브 트리가 없거나 왼쪽 서브 트리가 없는 경우
+    if numbers[s_idx] > numbers[e_idx-1] or numbers[s_idx] < numbers[s_idx+1]:
+        dfs(s_idx+1, e_idx) # for문을 거치지 않고 바로 다시 탐색
+        print(numbers[s_idx])
+        return
+    
+    for i in range(s_idx, e_idx): # 오른쪽 서브 트리가 어디인지 탐색
+        if numbers[i] > numbers[s_idx]: # 오른쪽 서브 트리 시작점을 발견하면
+            break # for문 종료
+    
+    dfs(s_idx+1, i)
+    dfs(i, e_idx)
     print(numbers[s_idx])
 
-
-postorder(0, len(numbers)-1)
-    
+dfs(0, len(numbers))

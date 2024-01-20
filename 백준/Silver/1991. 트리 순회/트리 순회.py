0ee2467abfd800fcf35464671345b3516bba8f00
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-20 18:58:03 +0900

    [Silver I] Title: 트리 순회, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,39 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+class Node:
+    def __init__(self, data, left, right):
+        self.data = data
+        self.left = left
+        self.right = right
+    
+def preorder(node): # 전위 순회
+    print(node.data, end='')
+    if node.left != '.': preorder(tree[node.left])
+    if node.right != '.': preorder(tree[node.right])
+    
+def inorder(node): # 중위 순회
+    if node.left != '.': inorder(tree[node.left])
+    print(node.data, end='')
+    if node.right != '.': inorder(tree[node.right])
+
+def postorder(node): # 후위 순회
+    if node.left != '.': postorder(tree[node.left])
+    if node.right != '.': postorder(tree[node.right])
+    print(node.data, end='')
+
+
+n = int(input())
+tree = {} # 딕셔너리 이용
+
+for _ in range(n):
+    data, left, right = input().split()
+    tree[data] = Node(data, left, right)
+
+# 항상 A가 루트 노드
+preorder(tree['A'])
+print()
+inorder(tree['A'])
+print()
+postorder(tree['A'])

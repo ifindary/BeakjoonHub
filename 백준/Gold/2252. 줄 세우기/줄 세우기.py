30edd0e8dc58206ae38c37d3014928aea8f72206
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-24 19:21:56 +0900

    [Gold III] Title: 줄 세우기, Time: 184 ms, Memory: 41868 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,35 +1,31 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
 N, M = map(int, input().split()) # N 학생 수, M 힌트
 graph = [[] for _ in range(N+1)] # 대상:[선행 조건] 형태
 line = [] # 줄을 선 순서
-
-indegree = [0]*(N+1) # 진입 차수
+visited = [False]*(N+1)
 
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a].append(b) # a가 줄을 서려면 b가 먼저 서야 한다
-    indegree[b] += 1
+    graph[b].append(a) # b가 줄을 서려면 a가 먼저 서야 한다
 
-def tepology_sort():
-    line = [] # 줄을 서는 순서
-    q = deque() # 줄을 설 수 있는 사람(진입 차수가 0) 
+def dfs(v):
+    if visited[v] == True: # 이미 방문한 노드면 넘기기
+        return
+    
+    visited[v] = True
 
-    for i in range(1, N+1):
-        if not indegree[i]:
-            q.append(i)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
 
-    while q:
-        now = q.popleft()
-        line.append(now)
+    # 여기까지 왔으면 이제 줄을 서도 된다는 뜻
+    line.append(v)
 
-        for i in graph[now]:
-            indegree[i] -= 1
-            if not indegree[i]:
-                q.append(i)
+for i in range(1, N+1):
+    dfs(i)
 
-    print(*line)
-
-tepology_sort()
+print(*line)

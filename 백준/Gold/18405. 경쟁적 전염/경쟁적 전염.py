e3234900e6e766e4627d4ef3efb2699798baf546
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-26 00:23:05 +0900

    [Gold V] Title: 경쟁적 전염, Time: 164 ms, Memory: 34676 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -3,47 +3,31 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-temp = [input().rstrip().split() for _ in range(N)]
+graph = [list(map(int, input().split())) for _ in range(N)]
 S, X, Y = map(int, input().split())
-
-graph = [[0]*N for _ in range(N)]
 
 q = deque()
 
-# temp를 바로 int로 받는 법이 기억 안 나서 임시 처리
 for i in range(N):
     for j in range(N):
-        graph[i][j] = int(temp[i][j])
-
         if graph[i][j] != 0:
-            q.append([graph[i][j], i, j])
+            q.append([graph[i][j], i, j, 0]) # 바이러스 번호, x 좌표, y 좌표, 시간
 
 q = deque(sorted(q))
 
-now = 0
-wait = deque()
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+while q:
+    a, x, y, t = q.popleft()
+    if t >= S:
+        break
 
-while now < S:
-    while q:
-        temp = q.popleft()
+    for i in range(4):
+        if 0 <= x+dx[i] < N and 0 <= y+dy[i] < N and graph[x+dx[i]][y+dy[i]] == 0:
+            graph[x+dx[i]][y+dy[i]] = a
 
-        a = temp[0]
-        x = temp[1]
-        y = temp[2]
-
-        # a: 지금 탐색 중인 바이러스. x, y: 현재 탐색 중인 위치
-        for i in range(4):
-            if 0 <= x+dx[i] < N and 0 <= y+dy[i] < N and graph[x+dx[i]][y+dy[i]] == 0:
-                graph[x+dx[i]][y+dy[i]] = a
-                wait.append([a, x+dx[i], y+dy[i]])
-
-    while wait:
-        q.append(wait.popleft())
-
-    now += 1
-
+            if t+1 < S:
+                q.append([a, x+dx[i], y+dy[i], t+1])
 
 print(graph[X-1][Y-1])

58ff514a413f9f5871ae57eadde345e7bbad4910
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-28 23:21:11 +0900

    [Silver III] Title: 01타일, Time: 236 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -6,10 +6,7 @@ N = int(input())
 a = 1
 b = 1
 
-for _ in range(3, N+1):
-    a, b = b%15746, (a + b)%15746
+for _ in range(2, N+1):
+    a, b = b, (a + b)%15746
 
-if N == 1:
-    print(1)
-else:
-    print((a + b)%15746)
+print(b)

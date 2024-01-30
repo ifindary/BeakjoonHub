eeac023214da66b4ae6901c64176236c6159a699
Yeon <154266737+ifindary@users.noreply.github.com>
2024-01-30 22:31:21 +0900

    [Gold V] Title: 동전, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -3,16 +3,20 @@ input=sys.stdin.readline
 
 T = int(input()) # 테스트 케이스 개수
 
+def count(coins, M):
+  dp = [0]*(M+1) # 결과를 저장할 테이블
+  dp[0] = 1
+
+  for coin in coins:
+    for i in range(coin, M+1):
+      dp[i] += dp[i-coin]
+
+  print(dp[M])
+
+
 for _ in range(T):
   N = int(input()) # 동전 가지 수
   coins = list(map(int, input().split())) # 동전 금액
   M = int(input()) # 만들어야 할 금액
 
-  dp = [0]*(M+1) # 결과를 저장할 테이블
-  dp[0] = 1
-
-  for coin in coins:
-    for j in range(coin, M+1):
-      dp[j] += dp[j-coin]
-
-  print(dp[M])
+  count(coins, M)

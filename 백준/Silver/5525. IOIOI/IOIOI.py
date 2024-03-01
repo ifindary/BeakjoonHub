828c6606884235779306a1f2c2e7bdc2dc3ed71f
Yeon <154266737+ifindary@users.noreply.github.com>
2024-03-01 15:53:43 +0900

    [Silver I] Title: IOIOI, Time: 284 ms, Memory: 34464 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -7,12 +7,22 @@ def ioioi():
     M = int(input())
     S = input().rstrip()
 
-    P = 'IO'*N + 'I'
     ans = 0
+    i = 0
+    count = 0
 
-    for i in range(M-len(P)+1):
-        if S[i:i+len(P)] == P:
-            ans += 1
+    while i < (M-1):
+        if S[i:i+3] == 'IOI':
+            i += 2
+            count += 1
+
+            if count == N:
+               ans += 1
+               count -= 1
+
+        else:
+            i += 1
+            count = 0
 
     print(ans)
 
